614bb5051f1ddd341edd483c5e65ccf7ef05d7c2
victor allisson <victor.allisson95@gmail.com>
2023-11-25 07:02:39 -0300

    Adcionado controlador e intermediario para login e autenticacao

---
+++ src/intermediarios/autenticador.js
@@ -0,0 +1,41 @@
+const knex = require('../conexao');
+const {senhaJwt} = require('../../senhaJwt')
+const jwt = require('jsonwebtoken');
+const { config } = require('dotenv');
+
+const autenticaUsuario = async(req,res,next)=>{
+    const {authorization} = req.headers;
+
+    try {
+        if(!authorization){
+            return res.status(401).json({mensagem: 'Para acessar este recurso um token de autenticação válido deve ser enviado.'})
+        }
+    
+        const token = authorization.split(' ')[1]
+
+        if(!token){
+            return res.status(401).json({mensagem: 'Para acessar este recurso um token de autenticação válido deve ser enviado.'})
+        }
+
+        const {id} = jwt.verify(token ,senhaJwt);
+
+        const usuario = await knex('usuarios').where('id', id);
+
+        if(!usuario){
+            return res.status(401).json({mensagem: 'Para acessar este recurso um token de autenticação válido deve ser enviado.'})
+        }
+
+        req.usuario = usuario
+        next();
+
+    } catch (error) {
+        if(error.message === 'invalid token'){
+            return res.status(401).json({mensagem:'Para acessar este recurso um token de autenticação válido deve ser enviado.'});
+        }
+        // return res.status(500).json({mensagem:'Erro interno do servidor'})
+        console.log(error);
+    }
+}
+module.exports={
+    autenticaUsuario
+}
--- src/rotas.js
+++ src/rotas.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const { listarCategorias } = require('./controladores/listarcategorias');
+const { autenticaUsuario } = require('./intermediarios/autenticador');
+const { login } = require('./controladores/usuarios');
+
 
 const rotas = express();
 
 rotas.get('/categoria', listarCategorias);
 
+rotas.post('/login', login);
+rotas.use(autenticaUsuario)
+
+rotas.get('/usuario', (req,res)=>{
+    res.send('testando autenticador')
+})
 module.exports = rotas;
